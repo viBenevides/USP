46a6d4eecab646853ffbb950e3b3f39b2d62282e
viBenevides <vi.benevides@hotmail.com>
2016-04-21 19:27:42 -0300

    Inserćão da materia de ITC e trabalho 1

---
+++ ITC/Trabalho 1/trabalho_1.c
@@ -0,0 +1,26 @@
+//Trabalho 1 de Introducao a teoria de Computacao SCC0505;
+//Victor Benevides 8531491
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//Struct do automato;
+typedef struct automato{
+	//Inserir lista de estados;
+}
+
+//Struct de estado;
+typedef struct estado{
+	//Inserir simbolos;
+	//Inserir ponteiro estado;
+}
+
+//Struct do simbolo;
+typedef struct simbolo{
+	//Inserir char de simbolos;
+}
+
+int main(){
+
+	return 0;
+}
